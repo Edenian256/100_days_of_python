2e6f7a5d1aa2482b56431e8e44dc38bfc90d12cf
Edenian256 <77830218+Edenian256@users.noreply.github.com>
2021-09-21 15:37:51 +0000

    comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # new line and quates
-print("Day 1 - Python Print Function\n The function is declared like this:\nprint('what to print')")
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
 # string concatnation
 print("Hello " + "Abenezer")
+# indentation is key. use the code intelligence
+# error messages come in order
